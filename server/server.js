cf243aa38686ea884159bb842b0cd4cc3350d838
OgutuJoseph <onyango.joseph@yahoo.com>
2022-05-23 12:09:38 +0300

    Code format

--- server/server.js
+++ server/server.js
@@ -11,12 +11,12 @@ app.use(cors());
 let transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
-    type: "OAuth2",
-    user: process.env.EMAIL,
-    pass: process.env.WORD,
-    clientId: process.env.OAUTH_CLIENTID,
-    clientSecret: process.env.OAUTH_CLIENT_SECRET,
-    refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+        type: "OAuth2",
+        user: process.env.EMAIL,
+        pass: process.env.WORD,
+        clientId: process.env.OAUTH_CLIENTID,
+        clientSecret: process.env.OAUTH_CLIENT_SECRET,
+        refreshToken: process.env.OAUTH_REFRESH_TOKEN,
     },
 });
 transporter.verify((err, success) => {
