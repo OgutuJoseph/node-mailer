fc883f1a851da5531a6febc587c51dbd7bb78ce8
OgutuJoseph <onyango.joseph@yahoo.com>
2022-06-06 14:41:25 +0300

    Updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,131 +1,71 @@
 import { useState } from "react";
 import './App.css';
-import { Formik } from 'formik';
-import { DialogActions, FormControl, MenuItem, Box, Select, InputLabel, TextField, Button, Dialog, DialogContent, CircularProgress, Grid } from '@mui/material'
+import axios from 'axios';
+import { DialogActions, TextField, Button, Dialog, DialogContent, CircularProgress, Grid, Input } from '@mui/material'
 
 function App() {
 
-  //handle invoice data objects
-  const [invoiceData, setInvoiceData] = useState([{ ledger_id: '', price: '', quantity: '', total_price: '' }])
-  const handleInvoiceData = (i, e) => {
-    const newInvoiceData = [...invoiceData];
-    if (e.target.name === 'item') {
-      newInvoiceData[i][e.target.name] = (e.target.value);
-    } else { newInvoiceData[i][e.target.name] = e.target.value; }
-    // newInvoiceData[i].credit_account_id = creditAccount;
-    setInvoiceData(newInvoiceData);
-  }
+  const [userInput, setUser] = useState({
+    email: '',
+    subject: ''
+})
+const [file, setFile] = useState(null);
 
-  const addFormFields = () => {
-    setInvoiceData([...invoiceData, { ledger_id: '', price: '', quantity: '', total_price: '' }])
-  }
+const onInputChange = (e) => {
+    e.persist();
+    setUser({ ...userInput, [e.target.name]:e.target.value });
+    setFile(e.target.files[0])
+}
 
-  const removeFormFields = (i) => {
-    const newInvoiceData = [...invoiceData];
-    newInvoiceData.splice(i, 1);
-    setInvoiceData(newInvoiceData)
-  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const data = new FormData();
+
+    data.append('file', file)
+    data.append('customer_email', userInput.customer_email);
+    data.append('customer_names', userInput.customer_names);
+    data.append('reference', userInput.reference);
+
+    axios.post('http://localhost:3001/send', data)
+            .then((e) => { console.log('Success!') })
+            .catch((e) => { console.log('Error!') })
+}
 
   return (
     <div className="App">
       <div className="container">
-        <Formik
-          initialValues={{
-            reference: '',
-            customer_name: '',
-            customer_email: '',
-            description: '',
-            invoice_date: '',
-          }}
-          onSubmit={async values => {
-            values = {
-              ...values,
-              invoiceData: invoiceData
-            };
-            console.log('values: ', values); 
-            const response = await fetch("http://localhost:3001/send", {
-            method: "POST",
-            headers: {
-                "Content-type": "application/json",
-            },
-            body: JSON.stringify({ values }),
-            })
-            .then((res) => res.json())
-            .then(async (res) => {
-                const resData = await res;
-                console.log(resData);
-                if (resData.status === "success") {
-                alert("Message Sent");
-                } else if (resData.status === "fail") {
-                alert("Message failed to send");
-                }
-            })
-          }}
-        >
-          {({
-            errors,
-            handleBlur,
-            handleChange,
-            handleSubmit,
-            isSubmitting,
-            touched,
-            values
-          }) => (
-            <form onSubmit={handleSubmit}>
-              <br /><br />
-              <DialogContent dividers>
-                <Grid container spacing={2}>
-                  <Grid item xs={12} sm={12} md={3} lg={3} xl={3}>
-                    <TextField error={Boolean(touched.reference && errors.reference)} fullWidth helperText={touched.reference && errors.reference} label="Reference" name="reference" size="small" required onBlur={handleBlur} onChange={handleChange} type="text" value={values.reference} variant="outlined" />
-                  </Grid>
-                  <Grid item xs={12} sm={12} md={3} lg={3} xl={3}>
-                    <TextField error={Boolean(touched.customer_name && errors.customer_name)} fullWidth helperText={touched.customer_name && errors.customer_name} label="Customer Name" name="customer_name" size="small" required onBlur={handleBlur} onChange={handleChange} type="text" value={values.customer_name} variant="outlined" />
-                  </Grid>
-                  <Grid item xs={12} sm={12} md={3} lg={3} xl={3}>
-                  <TextField error={Boolean(touched.customer_email && errors.customer_email)} fullWidth helperText={touched.customer_email && errors.customer_email} label="Customer Email" name="customer_email" size="small" required onBlur={handleBlur} onChange={handleChange} type="email" value={values.customer_email} variant="outlined" />
-                  </Grid>
-                  <Grid item xs={12} sm={12} md={3} lg={3} xl={3}>
-                    <TextField error={Boolean(touched.invoice_date && errors.invoice_date)} fullWidth helperText={touched.invoice_date && errors.invoice_date} name="invoice_date" size="small" required onBlur={handleBlur} onChange={handleChange} type="date" value={values.invoice_date} variant="outlined" />
-                  </Grid>
+        <form onSubmit={handleSubmit}>
+          <h2 style={{ textAlign: 'center' }}>Node Mailer</h2>
+          <br />
+          <div className="container col-8">
+            <DialogContent dividers>
+              <Grid container spacing={2}>
+                <Grid item xs={12} sm={12} md={3} lg={3} xl={3}>
+                  <TextField label="Customer Email" name="customer_email" size="small" required onChange={onInputChange} type="email" value={userInput.customer_email} variant="outlined" />
                 </Grid>
-                <br />
-                <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
-                  <TextField error={Boolean(touched.description && errors.description)} fullWidth helperText={touched.description && errors.description} label="Description" name="description" size="small" required onBlur={handleBlur} onChange={handleChange} type="text" value={values.description} variant="outlined" />
+                <Grid item xs={12} sm={12} md={3} lg={3} xl={3}>
+                  <TextField label="Customer Name" name="customer_names" size="small" required onChange={onInputChange} type="text" value={userInput.customer_names} variant="outlined" />
                 </Grid>
-              </DialogContent>
-              <DialogContent dividers>
-                <Grid container spacing={2}>
-                  <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
-                    {invoiceData.map((invoice, index) => (
-                      <Grid item xs={12} sm={12} md={12} lg={12} xl={12} key={index}>
-                        <TextField fullWidth label="Item" name="ledger_id" size="small" required onBlur={handleBlur} onChange={e => handleInvoiceData(index, e)} type="number" value={invoice.ledger_id || ''} variant="outlined" />
-                        <TextField fullWidth label="Price" name="price" size="small" required onBlur={handleBlur} onChange={e => handleInvoiceData(index, e)} type="number" value={invoice.price || ''} variant="outlined" />
-                        <TextField fullWidth label="Quantity" name="quantity" size="small" required onBlur={handleBlur} onChange={e => handleInvoiceData(index, e)} type="number" value={invoice.quantity || ''} variant="outlined" />
-                        <TextField fullWidth label="Total Price" name="total_price" size="small" required onBlur={handleBlur} onChange={e => handleInvoiceData(index, e)} type="number" value={invoice.total_price || ''} variant="outlined" />
-                        {
-                          index ?
-                            <Box>
-                              <Button type="button" variant="contained" color="primary" size="small" onClick={() => removeFormFields(index)}>Remove Item</Button>
-                            </Box>
-                            : null
-                        }
-                        <hr />
-                      </Grid>
-                    ))}
-                    <div >
-                      <Button type="button" variant="contained" color="primary" size="small" onClick={() => addFormFields()}>Add Item</Button>
-                    </div>
-                  </Grid>
+                <Grid item xs={12} sm={12} md={3} lg={3} xl={3}>
+                <TextField label="Reference" name="reference" size="small" required onChange={onInputChange} type="text" value={userInput.reference} variant="outlined" />
                 </Grid>
-              </DialogContent>
-              <DialogActions>
-                <div >
-                  <Button disabled={isSubmitting} type="submit" variant="contained" color="primary" size="small">Submit</Button>
-                </div>
-              </DialogActions>
-            </form>
-          )}
-        </Formik>
+                <Grid item xs={12} sm={12} md={3} lg={3} xl={3}>
+                  <TextField  name="invoice_date" size="small" required onChange={onInputChange} type="date" variant="outlined" />
+                </Grid>
+              </Grid>
+              <br />
+              <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+                <Input  label="Invoice" size="small" required  onChange={onInputChange} type="file"  variant="outlined" />
+              </Grid>
+            </DialogContent>
+            <DialogActions>
+              <div >
+                <Button type="submit" variant="contained" color="primary" size="small">Submit</Button>
+              </div>
+            </DialogActions>
+          </div>
+        </form>
       </div>
     </div>
   );
